bc7469802dfffd8a6cf3fdd77b4bc5bfa2adc53b
Oliver Grisha LORENZO FELIPE <lorenzo.felipe@policia.es>
2021-11-12 13:16:02 +0100

    fromGroup with ngSubmit works

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AuthRoutingModule } from './auth-routing.module';
 
@@ -19,7 +20,7 @@ import { MatButtonModule } from '@angular/material/button';
   ],
   imports: [
     CommonModule, AuthRoutingModule, MatFormFieldModule, MatAutocompleteModule,
-    MatInputModule, MatIconModule, MatButtonModule
+    MatInputModule, MatIconModule, MatButtonModule, ReactiveFormsModule
   ]
 })
 export class AuthModule { }
--- src/app/auth/components/register-page/register-page.component.ts
+++ src/app/auth/components/register-page/register-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-register-page',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterPageComponent implements OnInit {
 
+  registerForm = new FormGroup({
+    name: new FormControl(''),
+    paddleLevel: new FormControl(''),
+    email: new FormControl(''),
+    password: new FormControl(''),
+    passwordConfirmation: new FormControl(''),
+  });
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onRegisterSubmit() {
+    console.log("Register form submitted", this.registerForm.value);
   }
 
 }
