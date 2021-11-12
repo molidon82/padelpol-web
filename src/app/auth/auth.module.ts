eecc2de6dbed0293fe5331fd3f6cc50e1e839fd8
Oliver Grisha LORENZO FELIPE <lorenzo.felipe@policia.es>
2021-11-12 12:42:47 +0100

    registerPageComponent html added

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -2,16 +2,24 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
+
+// Components 
 import { RegisterPageComponent } from './components/register-page/register-page.component';
 
+// Angular material
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
     RegisterPageComponent
   ],
   imports: [
-    CommonModule,
-    AuthRoutingModule
+    CommonModule, AuthRoutingModule, MatFormFieldModule, MatAutocompleteModule,
+    MatInputModule, MatIconModule, MatButtonModule
   ]
 })
 export class AuthModule { }
