d7f690a64893f87b92c6bc097777032301006368
Oliver Grisha LORENZO FELIPE <lorenzo.felipe@policia.es>
2021-11-12 12:09:36 +0100

    unused component removed

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -1,10 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AuthComponent } from './auth.component';
 import { RegisterPageComponent } from './components/register-page/register-page.component';
 
 const routes: Routes = [
-  { path: '', component: AuthComponent },
   { path: 'registro', component: RegisterPageComponent }
 ];
 
