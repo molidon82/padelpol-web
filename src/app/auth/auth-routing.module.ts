d0a179c2af6f8cc4987c4484b70c50f9ed262582
Oliver Grisha LORENZO FELIPE <lorenzo.felipe@policia.es>
2021-11-12 12:03:26 +0100

    registerPageComponent added

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthComponent } from './auth.component';
+import { RegisterPageComponent } from './components/register-page/register-page.component';
 
-const routes: Routes = [{ path: '', component: AuthComponent }];
+const routes: Routes = [
+  { path: '', component: AuthComponent },
+  { path: 'registro', component: RegisterPageComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
 import { AuthComponent } from './auth.component';
+import { RegisterPageComponent } from './components/register-page/register-page.component';
 
 
 @NgModule({
   declarations: [
-    AuthComponent
+    AuthComponent,
+    RegisterPageComponent
   ],
   imports: [
     CommonModule,
