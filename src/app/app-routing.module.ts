d885c54ab9847ef92c3dba1aa25de4740d8f30ec
Oliver Grisha LORENZO FELIPE <lorenzo.felipe@policia.es>
2021-11-16 12:29:08 +0100

    gamesModule added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) }
+  { path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) },
+  { path: 'partidos', loadChildren: () => import('./games/games.module').then(m => m.GamesModule) }
 ];
 
 @NgModule({
