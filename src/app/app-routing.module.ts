b40b429c0e1259079b0462d9cd3fe6b118e232a5
Oliver Grisha LORENZO FELIPE <lorenzo.felipe@policia.es>
2021-11-17 09:55:46 +0100

    isAuthenticatedGuard added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { appRoutes } from './app-routes';
+import { IsAuthenticatedGuard } from './core/guards/is-authenticated.guard';
 
 const routes: Routes = [
   { path: appRoutes.authModule, loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) },
-  { path: appRoutes.gamesModule, loadChildren: () => import('./games/games.module').then(m => m.GamesModule) }
+  { path: appRoutes.gamesModule, loadChildren: () => import('./games/games.module').then(m => m.GamesModule), canActivate: [IsAuthenticatedGuard] }
 ];
 
 @NgModule({
---
+++ src/app/core/guards/is-authenticated.guard.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import { SessionStorageService } from '../services/session-storage.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class IsAuthenticatedGuard implements CanActivate {
+
+  constructor(private sessionStorageService: SessionStorageService) { }
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    return (this.sessionStorageService.getItem("token") != null);
+  }
+
+}
