3e9617d0370303a05ca0b63541fecb2cc5610aa4
Oliver Grisha LORENZO FELIPE <lorenzo.felipe@policia.es>
2021-11-17 13:16:02 +0100

    hide header and footer when authModule

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { appRoutes } from './app-routes';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'padelpol-web';
+
+  authModulePath = appRoutes.authModule;
+
+  constructor(public router: Router) { }
 }
