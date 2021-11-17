2cccde0448276731bf37cc7ea4d4dbbd5b2b346e
Oliver Grisha LORENZO FELIPE <lorenzo.felipe@policia.es>
2021-11-17 13:35:20 +0100

    redirect to login if not exists the path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { appRoutes } from './app-routes';
+import { authRoutes } from './auth/auth-routes';
 import { IsAuthenticatedGuard } from './core/guards/is-authenticated.guard';
 
 const routes: Routes = [
   { path: appRoutes.authModule, loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) },
-  { path: appRoutes.gamesModule, loadChildren: () => import('./games/games.module').then(m => m.GamesModule), canActivate: [IsAuthenticatedGuard] }
+  { path: appRoutes.gamesModule, loadChildren: () => import('./games/games.module').then(m => m.GamesModule), canActivate: [IsAuthenticatedGuard] },
+  { path: '**',  redirectTo: `/${appRoutes.authModule}/${authRoutes.loginPage}`, pathMatch: 'full' }
 ];
 
 @NgModule({
