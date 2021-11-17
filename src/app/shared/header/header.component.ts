ade77f51b5d487dec3e1d4d7ad560b3dd8363015
Oliver Grisha LORENZO FELIPE <lorenzo.felipe@policia.es>
2021-11-17 13:27:55 +0100

    header improved

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { appRoutes } from 'src/app/app-routes';
+import { authRoutes } from 'src/app/auth/auth-routes';
+import { User } from 'src/app/core/models/user';
+import { AuthService } from 'src/app/core/services/auth.service';
+import { SessionStorageService } from 'src/app/core/services/session-storage.service';
 
 @Component({
   selector: 'app-header',
@@ -8,13 +13,21 @@ import { Router } from '@angular/router';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor(public router: Router) { }
+  user: User | null;
+
+  constructor(public router: Router, private authService: AuthService, private sessionStorageService: SessionStorageService) { }
 
   ngOnInit(): void {
+    this.user = this.authService.getCurrentUser();
+  }
+
+  logOut() {
+    this.sessionStorageService.removeItem("token");
+    this.router.navigate([`${appRoutes.authModule}/${authRoutes.loginPage}`]);
   }
 
   openShowUserDialog() {
-    
+
   }
 
 }
